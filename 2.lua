c58e0b1f16e553a9930eddcef69095b60ff5fc15
Null <108587662+null-ss@users.noreply.github.com>
2022-07-02 10:51:46 -0700

    Create 2.lua

---
+++ 2.lua
@@ -0,0 +1,8 @@
+local port = CFrame.new(-794.5, -168.975, -1062.61)  
+local root = game.Players.LocalPlayer.Character.HumanoidRootPart
+
+root.CFrame = port
+wait(.1)
+root.CFrame = port
+wait(.1)
+root.CFrame = port
