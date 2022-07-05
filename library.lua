eb91056347a0965e14b430b5adab5491532c9432
Null <108587662+null-ss@users.noreply.github.com>
2022-07-05 16:05:28 -0700

    Create library.lua

---
+++ library.lua
@@ -0,0 +1,16 @@
+--[[
+SNOWBANK SCRIPTS
+VERSION 1.3.2
+MADE BY doom#1000 \ nullzone#4318 | icy#1000
+discord.gg/luau
+]]--
+
+local SnowbankScripts = {}
+
+local discord = 'https://discord.gg/luau'
+
+function SnowbankScripts.newTab(name) end
+function SnowbankScripts.newToggle() end
+function SnowbankScripts.newButton() end
+
+return SnowbankScripts
